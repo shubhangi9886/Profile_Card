f9f8c345c71ac083d440fb4b915337839fbf5263
shubhangi <shubhangi18@navgurukul.org>
2020-08-07 20:09:30 +0530

    updated

--- src/Component/Practice.js
+++ src/Component/Practice.js
@@ -1,47 +1,34 @@
-import React, { Component } from "react";
-import { Card, withStyles, } from "@material-ui/core";
-import { CardText, CardBody, CardTitle, Button } from 'reactstrap';
+import React from "react";
+import { Card, } from "@material-ui/core";
+import { CardText, CardTitle, } from 'reactstrap';
 import demo from "./cute-girl-baby.jpg";
 
-const styles = theme => ({
-    root: {
-        height: 300,
-        width: '20%',
-        marginTop: '10%',
-        marginLeft: '40%',
-        border: '1px solid black',
-    },
-})
-
-class CardComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: 'shubhangi',
-            profile: demo,
-            Description: 'she is very cute Girl'
-        };
-    }
-
-    render() {
-        const { classes, } = this.props;
-        return (
-            <div>
-                <Card className={classes.root}>
-                    <div className={classes.img}>
-                        <img  src={this.state.profile}
-                            style={{ width: "170px", borderRadius: "50%", height: "170px", cursor: "pointer" , marginTop:'5%' }}
-                            alt="img" />
-                    </div>
-                    <CardBody>
-                        <CardTitle>{this.state.name}</CardTitle>
-                        <CardText>{this.state.Description}</CardText>
-                        <Button>Button</Button>
-                    </CardBody>
-                </Card>
-            </div>
-        );
-    }
+function CardDetail() {
+    return (
+        <div>
+            <Card className='root'
+                style={{
+                    height: 300,
+                    width: '20%', marginTop: '10%',
+                    marginLeft: '40%',
+                    border: '1px solid black',
+                }}>
+                <div>
+                    <img src={demo}
+                        style={{
+                            width: "170px",
+                            borderRadius: "50%",
+                            height: "170px",
+                            cursor: "pointer",
+                            marginTop: '5%'
+                        }}
+                        alt="" />
+                </div>
+                <CardTitle><h1>Shubhangi</h1></CardTitle>
+                <CardText>She is a Programmer. Now she is doing React.js.</CardText>
+            </Card>
+        </div>
+    );
 }
 
-export default withStyles(styles)(CardComponent);
+export default (CardDetail);
---
+++ src/Component/data.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        name: 'Shubhangi',
+        profile: '/home/shubhangi/Documents/practice/src/Component/cute-girl-baby.jpg',
+        Discribtion: 'She is progreammmer and now she is doing React'
+    },
+    {
+        name: 'Komal',
+        profile: '',
+        Discribtion: 'She is progreammmer and now she is doing React'
+    },
+    {
+        name: 'Pooja',
+        profile: '',
+        Discribtion: 'She is progreammmer and now she is doing React'
+    }
+]
