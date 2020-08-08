318d7cb2de7f761a66a30af1810d31379355a840
shubhangi <shubhangi18@navgurukul.org>
2020-08-08 16:02:55 +0530

    updated

--- src/Component/Practice.js
+++ src/Component/Practice.js
@@ -1,34 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, } from "@material-ui/core";
 import { CardText, CardTitle, } from 'reactstrap';
 import demo from "./cute-girl-baby.jpg";
 
-function CardDetail() {
-    return (
-        <div>
-            <Card className='root'
-                style={{
-                    height: 300,
-                    width: '20%', marginTop: '10%',
-                    marginLeft: '40%',
-                    border: '1px solid black',
-                }}>
-                <div>
-                    <img src={demo}
-                        style={{
-                            width: "170px",
-                            borderRadius: "50%",
-                            height: "170px",
-                            cursor: "pointer",
-                            marginTop: '5%'
-                        }}
-                        alt="" />
-                </div>
-                <CardTitle><h1>Shubhangi</h1></CardTitle>
-                <CardText>She is a Programmer. Now she is doing React.js.</CardText>
-            </Card>
-        </div>
-    );
+class CardDetail extends Component {
+    render() {
+        const name = 'shubhangi'
+        const description = 'She is a Programmer. Now she is doing React.js.'
+        const profile = demo
+
+        return (
+            <div>
+                <Card style={{ height: 300, width: '20%', marginTop: '10%', marginLeft: '40%', border: '1px solid black', }}>
+                    <div>
+                        <img src={profile}
+                            style={{
+                                width: "170px", borderRadius: "50%", height: "170px", cursor: "pointer", marginTop: '5%'
+                            }} alt="" />
+                    </div>
+                    <CardTitle><h1>{name}</h1></CardTitle>
+                    <CardText>{description}</CardText>
+                </Card>
+            </div>
+        )
+    };
 }
 
-export default (CardDetail);
+export default (CardDetail); 
